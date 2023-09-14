4c955e82c866450b9b98c49250063b4d15d6d60a
narayana5e4 <74009677+narayana5e4@users.noreply.github.com>
2023-09-14 22:30:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,14 +4,11 @@ terraform {
   required_providers {
     google ={
     version = "4.82.0"
-}
-
-  }
-
+        }
+     }
 }
 resource "google_storage_bucket" "test" {
      name              = "testingforgithubatcions13"
      location          = "EU"
-    force_destroy      = true 
-  
+    force_destroy      = true  
 }
